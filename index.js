448ef451cef3b1c03a035ffc3ea3d33ce975e1cb
Wenbo <hi@arcto.me>
2021-11-16 20:00:07 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import { Transformer } from 'koaw-js'
-import React from 'react';
+import { createElement } from 'react';
 import ReactDOMServer from 'react-dom/server'
 import { getAssetFromKV, NotFoundError, MethodNotAllowedError } from '@cloudflare/kv-asset-handler'
-import { StaticRouter } from 'react-router-dom/server.js'
-import { matchPath } from 'react-router-dom'
+import { match } from 'path-to-regexp'
+// import { StaticRouter } from 'react-router-dom/server.js'
+// import { matchPath } from 'react-router-dom'
+// import routes from './pages/routes'
 
 // Static file fetch, for server-rendering
 async function handleEvent(event) {
@@ -31,26 +33,38 @@ async function handleEvent(event) {
   }
 }
 
-export function ReactSSRMiddleware(event, App, routes) {
+export function ReactSSRMiddleware(event, routes) {
   return async (ctx) => {
     try {
       const response = await handleEvent(event).catch(e => { console.log(e) });
       ctx.res = await Transformer.responseToCtx(response).catch(e => console.log(e));
       let indexHTML = ctx.res.body;
       if (ctx.res.headers['content-type'] && ctx.res.headers['content-type'].includes('text/html')) {
-        const matchRoute = routes.find(r => matchPath(new URL(event.request.url).pathname, r.path))
+        // const matchRoute = routes.find(r => matchPath(ctx.req.url.pathname, r.path))
+        const matchRoute = routes.find(r => {
+          let matchTester = match(r.path);
+          if(matchTester(ctx.req.url.pathname)) return r;
+          return;
+        })
         let prefetchData = {};
-        let appHTML;
-        if (matchRoute && matchRoute.component && typeof matchRoute.component.fetchDataOnEdge === 'function') {
-          prefetchData = await matchRoute.component.fetchDataOnEdge();
+        // console.log(matchRoute.component)
+        let com;
+        if(typeof matchRoute.component === 'function') {
+          com = matchRoute.component();
+        } else {
+          com = matchRoute.component;
         }
-        const e = React.createElement;
-
-        appHTML = ReactDOMServer.renderToString(
-          e(StaticRouter, { location: event.request.url }, e(App, { serverProps: prefetchData })),
+        if(typeof com === 'object' && com.default) {
+          com = com.default
+        }
+        if (matchRoute && matchRoute.component && typeof matchRoute.component === 'function' && typeof matchRoute.component().fetchDataOnEdge === 'function') {
+          prefetchData = await matchRoute.component().fetchDataOnEdge();
+        }
+        let appHTML = ReactDOMServer.renderToString(
+          createElement(com, { serverProps: {data: prefetchData, location: ctx.req.url.pathname} })
         )
         // populate `#app` element with `appHTML`
-        ctx.res.body = indexHTML.replace('<!-- APP_PLACEHOLDER -->', appHTML).replace('<!-- SCRIPT_PLACEHOLDER -->', `<script>window.__INITIAL_DATA__ = ${JSON.stringify(prefetchData)}</script>`);
+        ctx.res.body = indexHTML.replace('<!-- APP_PLACEHOLDER -->', appHTML).replace('<!-- SCRIPT_PLACEHOLDER -->', `<script>window.__INITIAL_DATA__ = ${JSON.stringify({data: prefetchData, location: ctx.req.url.pathname})}</script>`);
       }
     } catch (e) {
       console.log('Error occurs in: ' + event.request.url);
