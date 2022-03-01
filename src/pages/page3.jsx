3ed6eacfba2de6071558f94fa5e61dcbb008d6b1
arctome <hi@arcto.me>
2022-03-01 08:50:50 +0000

    test: multi page render

---
+++ src/pages/page3.jsx
@@ -0,0 +1,8 @@
+export default function Page3(props) {
+    return (
+        <div>
+            <h3>This is page 3</h3>
+            <strong>No `getDataOnEdge` lifecycle.</strong>
+        </div>
+    )
+}
--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,7 @@
 import * as Page1 from "./pages/page1.jsx"
 import * as Page2 from "./pages/page2.jsx"
+import * as Page3 from "./pages/page3.jsx"
+
 
 const routes = [
     {
@@ -11,6 +13,11 @@ const routes = [
         name: "Page2",
         component: Page2,
         path: "/about"
+    },
+    {
+        name: "Page3",
+        component: Page3,
+        path: "/no-ssr"
     }
 ]
 
